c60f0b9ea09e2710cff5394a82c89df5bbc51e95
Murray David Neuzerling <murray@neuzerling.com>
2020-04-19 19:11:35 +1000

    Some undocumented route functions

--- R/routes.R
+++ R/routes.R
@@ -3,5 +3,9 @@ route_types <- function(user_id = determine_user_id(),
   PTVGET(request = "route_types", user_id = user_id, api_key = api_key)
 }
 
+routes <- function(user_id = determine_user_id(),
+                   api_key = determine_api_key()) {
+  PTVGET(request = "routes", user_id = user_id, api_key = api_key)
+}
 
 
