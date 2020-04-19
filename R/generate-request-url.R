75ca5af8b457ef02af3774c9e27b13956573cac0
Murray David Neuzerling <murray@neuzerling.com>
2020-04-19 18:49:26 +1000

    Processing httr responses

--- R/generate-request-url.R
+++ R/generate-request-url.R
@@ -1,6 +1,5 @@
 #' Generate a URL with devid and signature
 #'
-#' @param request Character for request/path, eg. "routes"
 #' @inheritParams PTVGET
 #'
 #' @return A complete URL character that can be queried with httr
@@ -10,9 +9,6 @@ generate_request_url <- function(request,
                                  user_id = determine_user_id(),
                                  api_key = determine_api_key()) {
 
-  base_url <- "http://timetableapi.ptv.vic.gov.au"
-  version <- 3 # This function is untested on other versions
-
   signature <- digest::hmac(
     key = api_key,
     object = glue::glue("/v{version}/{request}?devid={user_id}"),
@@ -20,10 +16,26 @@ generate_request_url <- function(request,
   )
 
   glue::glue(
-    base_url,
-    "/v{version}",
-    "/{request}",
+    generate_request_url_without_auth(),
     "?devid={user_id}",
     "&signature={toupper(signature)}"
+  )
+}
+
+#' Generate a URL without devid and signature
+#'
+#' @inheritParams PTVGET
+#'
+#' @return A complete URL character that can be queried with httr
+#'
+#' @keywords internal
+generate_request_url_without_auth <- function(request) {
+  base_url <- "http://timetableapi.ptv.vic.gov.au"
+  version <- 3 # This function is untested on other versions
+
+  glue::glue(
+    base_url,
+    "/v{version}",
+    "/{request}"
   )
 }
--- R/routes.R
+++ R/routes.R
@@ -2,3 +2,6 @@ route_types <- function(user_id = determine_user_id(),
                         api_key = determine_api_key()) {
   PTVGET(request = "route_types", user_id = user_id, api_key = api_key)
 }
+
+
+
