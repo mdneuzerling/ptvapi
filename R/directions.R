5821425272f68cfdfa856d5560bc85c22b617805
Murray David Neuzerling <murray@neuzerling.com>
2020-04-21 07:55:46 +1000

    Started directions function

---
+++ R/directions.R
@@ -0,0 +1,16 @@
+#' Directions for a given route
+#'
+#' @param route_id Integer. These can be listed and described with the `routes`
+#' function.
+#'
+#' @return directions
+#' @export
+#'
+directions <- function(route_id,
+                       user_id = determine_user_id(),
+                       api_key = determine_api_key()) {
+  request <- glue::glue("directions/route/{route_id}")
+  response <- PTVGET(request, user_id = user_id, api_key = api_key)
+  content <- response$content
+  content #//TODO
+}
