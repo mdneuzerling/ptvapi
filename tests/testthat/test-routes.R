f7b53df93b8e85226a9026aeaf4d86eedf21a89d
Murray David Neuzerling <murray@neuzerling.com>
2020-04-21 21:43:54 +1000

    Added test-routes

---
+++ tests/testthat/test-routes.R
@@ -0,0 +1,11 @@
+all_routes <- routes()
+
+test_that("Frankston train route can be found", {
+  # I can't think of any non-train routes, but just in case
+  frankston_train_routes <- all_routes %>%
+    dplyr::filter(
+      route_name == "Frankston",
+      route_type == translate_route_types("Train")
+    )
+  expect_equal(nrow(frankston_train_routes), 1)
+})
