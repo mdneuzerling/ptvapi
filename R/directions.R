849b623bf0e4b87a6ce209fce3bcbec9461130bf
Murray David Neuzerling <murray@neuzerling.com>
2020-04-21 21:03:44 +1000

    Added route type to directions, finished documentation

--- R/directions.R
+++ R/directions.R
@@ -1,41 +1,38 @@
 #' Directions for a given direction ID
 #'
-#' @param direction_id
+#' @param direction_id Integer. Try searching for a direction_id on a particular
+#'   route with `route_direction`.
+#' @param route_type Optionally filter results by a route type. A route type can
+#'   be provided either as a non-negative integer code, or as a character:
+#'   "Tram", "Train", "Bus", "Vline" or "Night Bus". Character inputs are not
+#'   case-sensitive. Use the `route_types` function to extract a vector of all
+#'   route types.
 #' @inheritParams PTVGET
 #'
-#' @return directions
+#' @inherit parse_directions_content return
 #' @export
 #'
 directions <- function(direction_id,
+                       route_type = NULL,
                        user_id = determine_user_id(),
                        api_key = determine_api_key()) {
   request <- glue::glue("directions/{direction_id}")
+  if (!is.null(route_type)) {
+    route_type <- translate_route_types(route_type)
+    request <- glue::glue(request, "/route_type/{route_type}")
+  }
   response <- PTVGET(request, user_id = user_id, api_key = api_key)
   content <- response$content
-
-  assert_correct_attributes(
-    names(content),
-    c("directions", "status")
-  )
-  parsed <- purrr::map_dfr(content$directions, tibble::as_tibble)
-
-  assert_correct_attributes(
-    colnames(parsed),
-    c("route_direction_description", "direction_id", "direction_name",
-      "route_id", "route_type")
-  )
-
-  parsed
-
+  parse_directions_content(content)
 }
 
 #' Directions for a given route_id
 #'
 #' @param route_id Integer. These can be listed and described with the `routes`
-#' function.
+#'   function.
 #' @inheritParams PTVGET
 #'
-#' @return directions
+#' @inherit parse_directions_content return
 #' @export
 #'
 route_directions <- function(route_id,
@@ -44,12 +41,32 @@ route_directions <- function(route_id,
   request <- glue::glue("directions/route/{route_id}")
   response <- PTVGET(request, user_id = user_id, api_key = api_key)
   content <- response$content
+  parse_directions_content(content)
+}
 
+#' Parse content of directions API call
+#'
+#' This function is designed to parse the content
+#'
+#' @param directions_content API response returned in the interior of a
+#' directions function.
+#'
+#' @return A tibble consisting of the following columns: \itemize{
+#' \item `direction_id`
+#' \item `direction_name`,
+#' \item `route_id`
+#' \item `route_type`
+#' \item `route_direction_description`
+#' }
+#'
+#' @export
+#'
+parse_directions_content <- function(directions_content) {
   assert_correct_attributes(
-    names(content),
+    names(directions_content),
     c("directions", "status")
   )
-  parsed <- purrr::map_dfr(content$directions, tibble::as_tibble)
+  parsed <- purrr::map_dfr(directions_content$directions, tibble::as_tibble)
 
   assert_correct_attributes(
     colnames(parsed),
@@ -57,6 +74,6 @@ route_directions <- function(route_id,
       "route_id", "route_type")
   )
 
-  parsed
-
+  parsed[c("direction_id", "direction_name", "route_id", "route_type",
+           "route_direction_description")]
 }
