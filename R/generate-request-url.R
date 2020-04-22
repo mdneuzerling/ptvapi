c3ef1b3a7ba1b2fa44f32a90a3566615afd8d3f0
Murray David Neuzerling <murray@neuzerling.com>
2020-04-23 08:17:28 +1000

    Generating wrong URL when using parameters

--- R/generate-request-url.R
+++ R/generate-request-url.R
@@ -5,21 +5,31 @@
 #' @return A complete URL character that can be queried with httr
 #'
 #' @keywords internal
+#'
 generate_request_url <- function(request,
                                  user_id = determine_user_id(),
                                  api_key = determine_api_key()) {
   version <- 3
 
+  # devid is a parameter, which means it goes on the end of the URL like so:
+  # url?param1=value&param2=value&param3=value
+  # If a URL already has a ? parameter, we join the devid with &
+  # Otherwise, we start appending parameters to a URL with ?
+  conjunction <- ifelse(grepl("\\?", request), "&", "?")
+  request_with_devid <- glue::glue(
+    "/v{version}/{request}{conjunction}devid={user_id}"
+  )
   signature <- digest::hmac(
     key = api_key,
-    object = glue::glue("/v{version}/{request}?devid={user_id}"),
+    object = request_with_devid,
     algo = "sha1"
   )
+  signature <- toupper(signature)
 
   glue::glue(
     generate_request_url_without_auth(request),
-    "?devid={user_id}",
-    "&signature={toupper(signature)}"
+    "{conjunction}devid={user_id}",
+    "&signature={signature}"
   )
 }
 
