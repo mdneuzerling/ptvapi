5662d1c862088dea4b155566c114d7c88ef23c05
Murray David Neuzerling <murray@neuzerling.com>
2020-04-23 08:17:39 +1000

    Added test for stops_on_route using direction_id

--- tests/testthat/test-stops.R
+++ tests/testthat/test-stops.R
@@ -12,6 +12,22 @@ test_that("Expected stops on Frankston train line", {
   expect_true("Cheltenham Station" %in% stops_on_frankston_line$stop_name)
 })
 
+test_that("Frankston train stops can be given a direction", {
+  frankston_route_directions <- route_directions(frankston_route_id)
+  frankston_direction_id <- frankston_route_directions %>%
+    filter(direction_name == "Frankston") %>%
+    pull(direction_id)
+  frankston_stops_with_directions <- stops_on_route(
+    route_id = frankston_route_id,
+    route_type = train_route_type,
+    direction = frankston_direction_id
+  )
+  frankston_train_ends <- frankston_stops_with_directions %>%
+    filter(stop_sequence == max(stop_sequence))
+  expect_equal(nrow(frankston_train_ends), 1)
+  expect_equal(frankston_train_ends$stop_suburb, "Frankston")
+})
+
 cheltenham_stop_id <- stops_on_frankston_line %>%
   filter(stop_name == "Cheltenham Station") %>%
   pull(stop_id)
