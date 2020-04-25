77fdaf709f8c7d672ab15e23b95c3e7277f89a9b
Murray David Neuzerling <murray@neuzerling.com>
2020-04-25 12:19:11 +1000

    Disabling lintr test due to path error

--- tests/testthat/test-lintr.R
+++ tests/testthat/test-lintr.R
@@ -1,3 +1,16 @@
-test_that("Package linting", {
-  lintr::expect_lint_free()
-})
+# This only works if I run `rcmdcheck::rcmdcheck()` from the console.
+# If I run this in the Build panel in RStudio, or through Github Actions, I get:
+#
+# invalid 'path' argument
+# Backtrace:
+#   1. lintr::expect_lint_free()
+#   2. lintr::lint_package(...)
+#   3. base::normalizePath(path, mustWork = FALSE)
+#   4. base::path.expand(path)
+#
+# This seems to be related to the exclusions: If I remove the # nolint tags in
+# ptv-verbs.R, everything is okay.
+
+# test_that("Package linting", {
+#   lintr::expect_lint_free()
+# })
