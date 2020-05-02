00bd460a2f44ac59da38ba233404fb934b6cf1c9
Murray David Neuzerling <murray@neuzerling.com>
2020-05-02 20:21:44 +1000

    Added unit test for disruption modes

---
+++ tests/testthat/test-disruptions.R
@@ -0,0 +1,8 @@
+test_that("Disruption modes contain expected disruptions", {
+  retrieved_disruption_modes <- disruption_modes()
+  expect_true("metro_train" %in% retrieved_disruption_modes)
+  expect_true("metro_tram" %in% retrieved_disruption_modes)
+  expect_true("ferry" %in% retrieved_disruption_modes)
+  expect_true("taxi" %in% retrieved_disruption_modes)
+  expect_true("general" %in% retrieved_disruption_modes)
+})
