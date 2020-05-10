1e4c05f968d7ae60fd6a191794eb1fdc91bd39f8
Murray David Neuzerling <murray@neuzerling.com>
2020-05-10 22:38:55 +1000

    Test for route_types in search_stops

--- tests/testthat/test-search-stops.R
+++ tests/testthat/test-search-stops.R
@@ -31,10 +31,24 @@ test_that("all results in search_stops relate to search term somehow", {
     nrow(
       dplyr::filter(
         ascot_vale,
-        !grepl("Ascot Vale", stop_name, ignore.case = TRUE),
+        !grepl("Ascot Vale", stop_name
+               , ignore.case = TRUE),
         !grepl("Ascot Vale", stop_suburb, ignore.case = TRUE)
       )
     ),
     0
+  )
+})
+
+test_that("search_stops can be filtered with multiple route types", {
+  expect_equal(
+    search_stops("South Yarra", route_types = c(0, 1)) %>%
+      pull(route_type) %>% unique %>% sort,
+    c(0, 1)
+  )
+  expect_equal(
+    search_stops("South Yarra", route_types = c(0, 2)) %>%
+      pull(route_type) %>% unique %>% sort,
+    c(0, 2)
   )
 })
