cfba026bdf091b9173e182338e0b9d5237f2cd8f
Murray David Neuzerling <murray@neuzerling.com>
2020-05-27 21:04:58 +1000

    Removed tests for route_types_cached

--- tests/testthat/test-route-types.R
+++ tests/testthat/test-route-types.R
@@ -10,12 +10,6 @@ test_that("All 5 route types present", {
   expect_true("Night Bus" %in% route_types_results)
 })
 
-test_that("route_types_cached is caching", {
-  route_types_cached()
-  expect_false(is.null(getOption("route_types")))
-  expect_identical(route_types_cached(), route_types())
-})
-
 test_that("Route translation are working", {
   expect_equal(translate_route_type(0), 0)
   expect_error(translate_route_type(99999))
