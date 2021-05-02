f4b50d76606e60220bc47fb188e9affd4fc60585
mdneuzerling <david@neuzerling.com>
2021-05-02 16:25:48 +1000

    Rebuild docs

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-# pkg.env is used for "private" package variables.
+# pkg_env is used for "private" package variables.
 # They're not truly private, but they are more difficult to access.
 # In our case, this is a useful place to cache results.
 pkg_env <- new.env(parent = emptyenv())
